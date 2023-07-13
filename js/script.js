e46aca5954f8d3fb761a7fb7815d63eacc1158c7
mashtalerleonid <mashtalerleonid@gmail.com>
2023-07-13 21:11:02 +0300

    fix1

--- js/script.js
+++ js/script.js
@@ -1,163 +1,139 @@
-const canvas = document.querySelector("#canvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight * 0.8;
-const ctx = canvas.getContext("2d");
+class ArrowAnimation {
+    constructor() {
+        this.canvas = document.querySelector("#canvas");
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight * 0.8;
+        this.ctx = this.canvas.getContext("2d");
+        this.startPoint = {};
+        this.ctx.lineWidth = 0;
+        this.ctx.lineCap = "round";
+        this.ctx.lineJoin = "round";
 
-const btnSpeedUp = document.querySelector("#speedUp");
-const btnSpeedDown = document.querySelector("#speedDown");
-const btnDistUp = document.querySelector("#distUp");
-const btnDistDown = document.querySelector("#distDown");
-const btnv1 = document.querySelector("#v1");
-const btnv2 = document.querySelector("#v2");
+        this.color = "rgb(187,230,34)";
 
-const distValue = document.querySelector(".distValue");
-const speedValue = document.querySelector(".speedValue");
+        this.intervalId = null;
+        this.interval = 20;
+        this.distBetweenArrows = 15 + 10;
+        this.pointer = 0;
+        this.k = 10;
+        this.angle = 0;
+    }
 
-btnSpeedUp.addEventListener("click", () => {
-    clearInterval(intervalInnerId);
-    interval -= 10;
-    start();
-    speedValue.innerHTML = interval;
-});
-btnSpeedDown.addEventListener("click", () => {
-    clearInterval(intervalInnerId);
-    interval += 10;
-    start();
-    speedValue.innerHTML = interval;
-});
-btnDistUp.addEventListener("click", () => {
-    clearInterval(intervalInnerId);
-    d += 10;
-    start();
-    distValue.innerHTML = d;
-});
-btnDistDown.addEventListener("click", () => {
-    clearInterval(intervalInnerId);
-    d -= 10;
-    start();
-    distValue.innerHTML = d;
-});
-btnv1.addEventListener("click", () => {
-    clearInterval(intervalInnerId);
-    transp1Arr = transp11Arr;
-    transp2Arr = transp22Arr;
-    transp3Arr = transp33Arr;
-    start();
-});
-btnv2.addEventListener("click", () => {
-    clearInterval(intervalInnerId);
-    transp1Arr = transp111Arr;
-    transp2Arr = transp222Arr;
-    transp3Arr = transp333Arr;
-    start();
-});
+    start() {
+        this.intervalInnerId = setInterval(this.animate.bind(this), this.interval);
+    }
 
-const params = {
-    
-};
-const color = "rgb(187,230,34)";
-const startPoint = { x: 200, y: 200 };
+    stop() {
+        clearInterval(this.intervalInnerId);
+    }
 
-ctx.translate(startPoint.x, startPoint.y);
-ctx.lineWidth = 5;
-ctx.lineCap = "round";
-ctx.lineJoin = "round";
+    setStartPoint(point) {
+        this.startPoint = point;
+    }
 
-let intervalInnerId = null;
-let interval = 60;
-let d = 50;
-let pointer = 0;
-const k = 10;
+    setLineWidth(width) {
+        this.ctx.lineWidth = width;
+    }
 
-distValue.innerHTML = d;
-speedValue.innerHTML = interval;
+    animate() {
+        const ctx = this.ctx;
+        const len = this.distBetweenArrows;
+        const d = this.distBetweenArrows;
+        let tr = 0;
+        let k = (0.33 * (d - this.pointer)) / d;
 
-const transp11Arr = [
-    10, 9, 8, 7, 6, 5, 4, 3, 2, 1,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 
-];
-const transp22Arr = [
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 
-    10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-];
-const transp33Arr = [
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-    10, 9, 8, 7, 6, 5, 4, 3, 2, 1,
-];
+        ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-const transp111Arr = [
-    10, 9, 8, 7, 6, 5, 4, 3, 2, 1,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 
+        ctx.save();
+        ctx.translate(this.startPoint.x, this.startPoint.y);
+        ctx.rotate(this.angle);
 
-];
-const transp222Arr = [
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 
-    10, 9, 8, 7, 6, 5, 4, 3, 2, 1,
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-    10, 9, 8, 7, 6, 5, 4, 3, 2, 1,
-];
-const transp333Arr = [
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 
-    10, 9, 8, 7, 6, 5, 4, 3, 2, 1,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-];
+        tr = 0.66 + k;
+        ctx.strokeStyle = `rgba(256,256,256,${tr})`;
+        ctx.lineWidth += 2;
+        ctx.beginPath();
+        ctx.moveTo(0 + this.pointer, -10);
+        ctx.lineTo(10 + this.pointer, 0);
+        ctx.lineTo(0 + this.pointer, 10);
+        ctx.stroke();
 
-let transp1Arr = transp11Arr;
-let transp2Arr = transp22Arr;
-let transp3Arr = transp33Arr;
+        ctx.strokeStyle = `rgba(187,230,34,${tr})`;
+        ctx.lineWidth -= 2;
+        ctx.beginPath();
+        ctx.moveTo(0 + this.pointer, -10);
+        ctx.lineTo(10 + this.pointer, 0);
+        ctx.lineTo(0 + this.pointer, 10);
+        ctx.stroke();
 
-function animate() {
-    const len = transp1Arr.length;
-       
-    ctx.strokeStyle = createRGBAColor(color, transp1Arr[pointer] / k);
-    ctx.clearRect(-startPoint.x, -startPoint.y, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.moveTo(0, -25);
-    ctx.lineTo(50, 0);
-    ctx.lineTo(0, 25);
-    ctx.stroke();
+        tr = 0.33 + k;
+        ctx.strokeStyle = `rgba(256,256,256,${tr})`;
+        ctx.lineWidth += 2;
+        ctx.beginPath();
+        ctx.moveTo(d + this.pointer, -10);
+        ctx.lineTo(10 + d + this.pointer, 0);
+        ctx.lineTo(d + this.pointer, 10);
+        ctx.stroke();
 
-    ctx.strokeStyle = createRGBAColor(color, transp2Arr[pointer] / k);
-    ctx.beginPath();
-    ctx.moveTo(d, -25);
-    ctx.lineTo(50 + d, 0);
-    ctx.lineTo(d, 25);
-    ctx.stroke();
+        ctx.strokeStyle = `rgba(187,230,34,${tr})`;
+        ctx.lineWidth -= 2;
+        ctx.beginPath();
+        ctx.moveTo(d + this.pointer, -10);
+        ctx.lineTo(10 + d + this.pointer, 0);
+        ctx.lineTo(d + this.pointer, 10);
+        ctx.stroke();
 
-    ctx.strokeStyle = createRGBAColor(color, transp3Arr[pointer] / k);
-    ctx.beginPath();
-    ctx.moveTo(2 * d, -25);
-    ctx.lineTo(50 + 2 * d, 0);
-    ctx.lineTo(2 * d, 25);
-    ctx.stroke();
+        tr = k;
+        ctx.strokeStyle = `rgba(256,256,256,${tr})`;
+        ctx.lineWidth += 2;
+        ctx.beginPath();
+        ctx.moveTo(2 * d + this.pointer, -10);
+        ctx.lineTo(10 + 2 * d + this.pointer, 0);
+        ctx.lineTo(2 * d + this.pointer, 10);
+        ctx.stroke();
 
-    pointer += 1;
+        ctx.strokeStyle = `rgba(187,230,34,${tr})`;
+        ctx.lineWidth -= 2;
+        ctx.beginPath();
+        ctx.moveTo(2 * d + this.pointer, -10);
+        ctx.lineTo(10 + 2 * d + this.pointer, 0);
+        ctx.lineTo(2 * d + this.pointer, 10);
+        ctx.stroke();
 
-    if (pointer === len) {
-        pointer = 0;
+        ctx.restore();
+
+        this.pointer += 1;
+
+        if (this.pointer === len) {
+            this.pointer = 0;
+        }
     }
 }
 
-function start() {
-    intervalInnerId = setInterval(animate, interval);
-}
+// --------------------------------------------------
 
-function stop() {
-    clearInterval(intervalInnerId);
-}
+const arrowAnimation = new ArrowAnimation();
+arrowAnimation.setStartPoint({ x: 200, y: 200 });
+arrowAnimation.setLineWidth(3);
+arrowAnimation.angle = 0;
+arrowAnimation.start();
 
-function createRGBAColor(rgbStr, transp) {
-    const rgbArr = rgbStr.split(",");
-    const r = rgbArr[0].split("(")[1];
-    const g = rgbArr[1];
-    const b = rgbArr[2].split(")")[0];
-    return `rgba(${r},${g},${b},${transp})`;
-}
+const btnSpeedUp = document.querySelector("#speedUp");
+const btnSpeedDown = document.querySelector("#speedDown");
+const speedValue = document.querySelector(".speedValue");
 
-start();
+btnSpeedUp.addEventListener("click", () => {
+    arrowAnimation.stop();
+    arrowAnimation.interval -= 1;
+    arrowAnimation.start();
+    speedValue.innerHTML = arrowAnimation.interval;
+});
+btnSpeedDown.addEventListener("click", () => {
+    arrowAnimation.stop();
+    arrowAnimation.interval += 1;
+    arrowAnimation.start();
+    speedValue.innerHTML = arrowAnimation.interval;
+});
+
+speedValue.innerHTML = arrowAnimation.interval;
+
+// arrowAnimation.angle = -Math.PI / 4;
